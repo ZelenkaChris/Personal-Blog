82b9721fe993c27da268365ba9e73f89cd564eaa
ZelenkaChris <zelenka.chris@gmail.com>
2018-06-25 22:47:17 -0700

    code cleanup

--- app2.js
+++ app2.js
@@ -1,15 +1,15 @@
-var password = "";
-
 var express = require('express');
 const path = require('path');
 var bodyParser = require('body-parser')
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+
+const pw = '';
 
 var app = express();
 
-app.use('/images', express.static(__dirname + '/images'));
 app.use('/font', express.static('/home/pi/node_modules/font-awesome'));
 app.use('/files', express.static(__dirname + '/files'));
-app.use('/favicon', express.static(__dirname + '/favicon'));
 app.use('/', express.static(__dirname + '/build'));
 app.use('/blog/images', express.static(__dirname + '/blog/images'));
 
@@ -20,34 +20,7 @@ app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
-var jade = require('jade');
-
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var db;
-
-var getIP = require('ipware')().get_ip;
-
-const hostname = '0.0.0.0';
 const port = 3000;
-
-function getDateTime() {
-  var date = new Date();
-
-  var hour = date.getHours();
-  hour = (hour < 10 ? "0" : "") + hour;
-
-  var min  = date.getMinutes();
-  min = (min < 10 ? "0" : "") + min;
-
-  var month = date.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
-
-  var day  = date.getDate();
-  day = (day < 10 ? "0" : "") + day;
-
-  return month + "/" + day + " " + hour + ":" + min;
-}
 
 app.get('/portfolio', function (req, res) {
   res.sendFile(path.join(__dirname, '/build', 'index.html'))
@@ -64,7 +37,7 @@ app.get('/blog/post', function(req, res) {
 app.post('/blog/post', function(req, res){
   var sentData = req.body.postingData;
 
-  if(sentData[3]['value'] === password){
+  if(sentData[3]['value'] === pw){
     db.collection("entries").insert({title: sentData[0]['value'],
     entry_date: sentData[1]['value'],
     body: sentData[2]['value']});
@@ -87,7 +60,7 @@ app.post('/blog/editor/:id', function(req, res) {
   var sentData = req.body.postingData;
 
   
-  if(sentData[3]['value'] === password){
+  if(sentData[3]['value'] === pw){
     var o_id = new mongo.ObjectID(req.params['id']);
     var query = {_id: o_id};
     
@@ -104,7 +77,6 @@ app.post('/blog/editor/:id', function(req, res) {
     console.log("Password used: " + sentData[3]['value']);
     res.end("pw")
   }
-  
 });
 
 
@@ -119,14 +91,12 @@ app.get('/api/entries/:id', function(req, res) {
   var query = {_id: o_id};
   db.collection("entries").find(query).toArray(function(err, docs) {
     res.json(docs);
-  });
-  
+  });  
 });
 
 MongoClient.connect('mongodb://localhost/blog', function(err, dbConnection) {
   db = dbConnection;
   var server = app.listen(port, function() {
-    var port = server.address().port;
-    console.log('Server running at http://$(hostname):$(port)/');
+    console.log('Server running');
   });
 });
